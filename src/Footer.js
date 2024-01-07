0d098f3baf7c0e377d92bc4e557bb1016782d8fd
yahie <psyya2@nottingham.ac.uk>
2024-01-07 23:34:10 +0000

    added footer to the bottom of page

--- src/Footer.js
+++ src/Footer.js
@@ -8,8 +8,8 @@ function Footer() {
                 <h3>My Website</h3>
                 <p>This is a place for your copyright or other info.</p>
                 <ul className="socials">
-                    <li><a href="https://github.com/yas305"><i className="your-icon-class">GH</i></a></li>
-                    <li><a href="https://www.instagram.com/_yahieali12"><i className="your-icon-class">IG</i></a></li>
+                <li><a href="https://www.instagram.com/_yahieali12"><i className="your-icon-class">IG</i></a></li>
+                <li><a href="https://github.com/yas305"><i className="your-icon-class">GH</i></a></li>
                 </ul>
             </div>
         </footer>
