50b1f4a48c4041d62af08893183f800d9c8fbe5a
yahie <psyya2@nottingham.ac.uk>
2024-01-10 17:44:09 +0000

    cv update

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './AboutMe.css';
 import myPortrait from './assets/yahie.jpg'; // replace with the path to your portrait
-import myCV from './assets/YahieAliCV.pdf';
+import myCV from './assets/YahieAliCV1.pdf';
 function AboutMeCard() {
   return (
     <div className="card">
@@ -10,7 +10,7 @@ function AboutMeCard() {
         <h2>About Me</h2>
         <p className='content'>My name is Yahie and i am a software engineer. I work mainly on the backend, but also have extensive knowledge on web Technologies. </p>
         
-        <a href={myCV} download="YahieAliCV.pdf" className="button">Download Resume</a> 
+        <a href={myCV} download="YahieAliCV1.pdf" className="button">Download Resume</a> 
                      </div>
     </div>
   );
