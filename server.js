820d1bed974a28e4710024df6576271f37912ba3
yahie <psyya2@nottingham.ac.uk>
2024-01-18 08:35:27 +0000

    fixed CV

--- server.js
+++ server.js
@@ -5,14 +5,10 @@ const app = express();
 // Serve static files from the React app build directory
 app.use(express.static(path.join(__dirname, 'build')));
 
-
 app.get('/cv', (req, res) => {
-    const pdfPath = path.join(__dirname, 'public', 'YahieAliCV.pdf'); 
+    const pdfPath = path.join(__dirname, 'build', 'YahieAliCV.pdf'); 
     res.sendFile(pdfPath);
 });
-
-
-
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
